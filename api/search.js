4a9c51f51d109c9e791a247a1da775239f12286d
sebastianangelovic619-sys <sebastian.angelovic619@gmail.com>
2026-09-15 19:47:42 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   /* =====================================================
      CARMATCH AI - FINAL MULTI MODEL AUTOMOTIVE BACKEND
 
